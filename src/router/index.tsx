6e076b064a4f18b1332e18246b44a5694153b27a
ruijadom <ruijadomingues@gmail.com>
2023-10-24 11:15:25 +0100

    refactor: create components and set pages

---
+++ src/router/index.tsx
@@ -0,0 +1,19 @@
+import { RouteObject } from "react-router-dom";
+
+import { HomePage } from "../pages/home.page";
+import { Layout } from "../components/layout";
+
+const normalRoutes: RouteObject = {
+  path: "*",
+  element: <Layout />,
+  children: [
+    {
+      index: true,
+      element: <HomePage />,
+    },
+  ],
+};
+
+const routes: RouteObject[] = [normalRoutes];
+
+export default routes;
